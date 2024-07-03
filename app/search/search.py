a5126ed1c731713b59a1305414609cea4f815dad
UR <eklavyarwt@gmail.com>
2024-07-03 10:45:24 +0530

    Added text preprocessors

--- app/search/search.py
+++ app/search/search.py
@@ -2,9 +2,11 @@ from sentence_transformers import util
 import numpy as np
 import torch
 from app.base.embeddings import model
+from app.util import preprocess_text
 
 
 def semantic_search(query: str, corpus_embeddings: torch.Tensor, top_n: int = 10):
+    query = preprocess_text(query)
     query_embedding = model.encode(query, convert_to_tensor=True)
     cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
     top_results = np.argpartition(-cos_scores, range(top_n))[:top_n]
